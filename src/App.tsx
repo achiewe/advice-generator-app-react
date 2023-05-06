be96d022054f139780698f2fbbf1e11f7341ae6c
achiewe <mamukashviliachi706@gmail.com>
2023-05-06 13:09:14 +0400

    create AdviceBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import GlobalStyles from "./GlobalStyles";
 import styled from "styled-components";
+import AdviceBox from "./components/AdviceBox";
 
 function App(): JSX.Element {
   return (
     <StyledMain>
       <GlobalStyles />
+      <AdviceBox />
     </StyledMain>
   );
 }
@@ -13,6 +15,7 @@ const StyledMain = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex-direction;
+  display: flex;
   justify-content: center;
   align-items: center;
   background-color: #202733;
---
+++ src/components/AdviceBox.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const AdviceBox = (): JSX.Element => {
+  return <BoxContainer></BoxContainer>;
+};
+
+export default AdviceBox;
+
+const BoxContainer = styled.div`
+  width: 100%;
+  max-width: 540px;
+  background-color: #313a48;
+  display: flex;
+  flex-direction: column;
+  padding: 40px 24px;
+`;
