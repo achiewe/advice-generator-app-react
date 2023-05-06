195b034774f924db0c2beaaef96a09b0c7f030d6
achiewe <mamukashviliachi706@gmail.com>
2023-05-06 13:02:46 +0400

    config app.tsx and add some images in folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import GlobalStyles from "./GlobalStyles";
+import styled from "styled-components";
 
 function App(): JSX.Element {
   return (
-    <div>
+    <StyledMain>
       <GlobalStyles />
-    </div>
+    </StyledMain>
   );
 }
 
+const StyledMain = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex-direction;
+  justify-content: center;
+  align-items: center;
+  background-color: #202733;
+  padding: 0 16px;
+`;
 export default App;
